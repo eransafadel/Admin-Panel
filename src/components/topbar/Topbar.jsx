2bad4ded62cb766bb5009c91dc2332011ce1f313
eransa <eransafadel@gmail.com>
2022-07-24 23:55:33 +0300

    style component user

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -21,7 +21,7 @@ const Topbar = () => {
                 <div className="topbarIconContainer">
                     <Settings/>
                 </div>
-                <img src="https://i.imgur.com/Lnt9K7l.jpeg" className="topAvatar" alt="top avatar"/>
+                <img src="https://media-exp1.licdn.com/dms/image/C5603AQEUjmnD7Lggvw/profile-displayphoto-shrink_800_800/0/1602768957092?e=1664409600&v=beta&t=IrRlvS7KCa2n6IvxpELyNPdfvuDZbGYhH75mAbsdDaM" className="topAvatar" alt="top avatar"/>
             </div>
         </div>
     </div>
--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -9,7 +9,18 @@ const User = () => {
         <button className="userAddButton">Create</button>
         </div>
         <div className="userContainer">
-          <div className="userShow"></div>
+          <div className="userShow">
+            <div className="userShowTop">
+            <img src="https://media-exp1.licdn.com/dms/image/C5603AQEUjmnD7Lggvw/profile-displayphoto-shrink_800_800/0/1602768957092?e=1664409600&v=beta&t=IrRlvS7KCa2n6IvxpELyNPdfvuDZbGYhH75mAbsdDaM" alt="" className="userShowImg" />
+                <div className="userShowTopTitle">
+                    <span className="userShowUsername">Eran Safadel</span>
+                    <span className="userShowUserTitle">Programmer</span>
+                </div>
+            </div>
+            <div className="userShowBottom">
+               
+            </div>
+          </div>
           <div className="userUpdate"></div>
         
       </div>
