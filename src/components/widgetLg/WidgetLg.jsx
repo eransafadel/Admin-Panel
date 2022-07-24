20813c69d2f492e7115f8d2bed80a2cbf23742a4
eransa <eransafadel@gmail.com>
2022-07-24 12:14:10 +0300

    finish latest transactions

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import './widgetLg.css';
+import "./widgetLg.css";
+import data from "./data/data";
 
-const WidgetLg = () => {
+ const  WidgetLg=()=> {
+  const Button = ({ type }) => {
+    return <button className={"widgetLgButton " + type}>{type}</button>;
+  };
+
+  const items = data.map((n,index)=>
+  <tr className="widgetLgTr">
+  <td className="widgetLgUser">
+    <img
+      src={n.image}
+      alt=""
+      className="widgetLgImg"
+    />
+    <span className="widgetLgName">{n.username}</span>
+  </td>
+  <td className="widgetLgDate">{n.date}</td>
+  <td className="widgetLgAmount">{n.amount}</td>
+  <td className="widgetLgStatus">
+    <Button type={n.status} />
+  </td>
+</tr> );
+
+
   return (
-    <div className="widgetLg">Large</div>
-  )
+    <div className="widgetLg">
+      <h3 className="widgetLgTitle">Latest transactions</h3>
+      <table className="widgetLgTable">
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+       {items}
+      </table>
+    </div>
+  );
 }
 
 export default WidgetLg;
---
+++ src/components/widgetLg/data/data.js
@@ -0,0 +1,37 @@
+const data = [
+  {
+    username: "Valeria Boltneva",
+    date: "24/07/2022",
+    amount: "$743.55",
+    status: "Approved",
+    image:
+      "https://images.pexels.com/photos/1034012/pexels-photo-1034012.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+
+  {
+    username: "Clara Krivitskiy",
+    date: "22/07/2022",
+    amount: "$600",
+    status: "Declined",
+    image:
+      "https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    username: "Maya Raz",
+    date: "20/07/2022",
+    amount: "$1231.90",
+    status: "Pending",
+    image:
+      "https://images.pexels.com/photos/1542085/pexels-photo-1542085.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    username: "Jack Winbow",
+    date: "19/07/2022",
+    amount: "$79.90",
+    status: "Approved",
+    image:
+      "https://images.pexels.com/photos/1559486/pexels-photo-1559486.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+];
+
+export default data;
