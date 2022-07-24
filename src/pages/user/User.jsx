b1c07b617bb5620093441764d8fe9cdb4e2f66cc
eransa <eransafadel@gmail.com>
2022-07-24 16:41:41 +0300

    style user component

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
 import "./user.css";
 
 const User = () => {
   return (
     <div className="user">
-        <div className="userTitleContainer">
-            <h1 className="userTitle">Edit User</h1>
-            <button className="userAddButton">Create</button>
+      <div className="userTitleContainer">
+        <h1 className="userTitle">Edit User</h1>
+        <button className="userAddButton">Create</button>
         </div>
+        <div className="userContainer">
+          <div className="userShow"></div>
+          <div className="userUpdate"></div>
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default User;
