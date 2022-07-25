8a936651d17efde9fdbb9bb97174e165d74f27f7
eransa <eransafadel@gmail.com>
2022-07-25 12:27:52 +0300

    finish update UserUpdate component

--- src/pages/user/userContainer/userUpdate/UserUpdate.jsx
+++ src/pages/user/userContainer/userUpdate/UserUpdate.jsx
@@ -3,21 +3,14 @@ import data from "../../data/data";
 import UserUpdateItem from "./userUpdateItem/UserUpdateItem";
 import "./userUpdate.css";
 import UserUpdateRight from './userUpdateRight/UserUpdateRight';
+import UserUpdateLeft from './userUpdateLeft/UserUpdateLeft';
 
 const UserUpdate = () => {
   return (
     <div className="userUpdate">
           <span className="userUpdateTitle">Edit</span>
           <form className="userUpdateForm">
-            <div className="userUpdateLeft">
-            
-              <UserUpdateItem title="Username" value={data.username}/>
-              <UserUpdateItem title="Full Name" value={data.fullname}/>
-              <UserUpdateItem title="Email" value={data.mail}/>
-              <UserUpdateItem title="Phone" value={data.phone}/>
-              <UserUpdateItem title="Address" value={data.location}/>
-            
-            </div>
+            <UserUpdateLeft/>
             <UserUpdateRight/>
           </form>
         </div>
