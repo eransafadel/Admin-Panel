8688b50927ec2a25bed5b6c3360b76e65509fbeb
eransa <eransafadel@gmail.com>
2022-07-26 12:09:41 +0300

    Reorganized folders

--- client/src/components/sidebar/MySidebar.jsx
+++ client/src/components/sidebar/MySidebar.jsx
@@ -4,15 +4,26 @@ import "./mySidebar.css";
 import items from "./data/data";
 import SidebarBuilder from "./SidebarBuilder";
 const MySidebar = () => {
-
   const data = items.map((n, index) => (
     <SidebarBuilder key={index} item={n.item} title={n.title} />
   ));
 
   return (
     <div className="sidebar">
-      <div className="sidebarWrapper">
-        {data}
+      <div className="sidebarWrapper">{data}</div>
+      <div className="bottom">
+        <div
+          className="colorOption"
+          onClick={""
+           // () => dispatch({ type: "LIGHT" })
+          }
+        ></div>
+        <div
+          className="colorOption"
+          onClick={""
+            //() => dispatch({ type: "DARK" })
+          }
+        ></div>
       </div>
     </div>
   );
